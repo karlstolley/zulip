f44f5e9922a09b697e43d6a0d7558fe2003c429b
Karl Stolley <karl@zulip.com>
2025-11-03 14:55:00 -0600

    WIP: Portico refactors.

---
+++ web/src/bundles/revamped_portico.ts
@@ -0,0 +1,6 @@
+import "./common.ts";
+import "../portico/header.ts";
+import "../portico/google-analytics.ts";
+import "../portico/portico_modals.ts";
+import "../portico/tippyjs.ts";
+import "tippy.js/dist/tippy.css";
