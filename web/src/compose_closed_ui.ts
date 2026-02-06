598575354eea7266cc5ee97b8607cc4af2f4bf2c
Karl Stolley <karl@zulip.com>
2026-02-06 10:56:16 -0600

    closed_compose: Track conversation narrows via class.

--- web/src/compose_closed_ui.ts
+++ web/src/compose_closed_ui.ts
@@ -16,6 +16,17 @@ import * as recent_view_util from "./recent_view_util.ts";
 import * as stream_data from "./stream_data.ts";
 import * as util from "./util.ts";
 
+// This is likely a temporary function for supporting both the
+// new skinned compose box in conversation views and the legacy
+// closed compose box in home views, etc.
+export function update_closed_compose_box_class_by_narrow(is_conversation_narrow: boolean): void {
+    if (is_conversation_narrow) {
+        $("#compose").addClass("composing-to-conversation-narrow");
+    } else {
+        $("#compose").removeClass("composing-to-conversation-narrow");
+    }
+}
+
 type RecipientLabel = {
     label_text: string;
     has_empty_string_topic?: boolean;
